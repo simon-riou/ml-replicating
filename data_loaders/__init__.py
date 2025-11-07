a1816ae5dfe824c52b887671e268ee7912793f0c
Simon Riou <simonriou3492@gmail.com>
2025-11-07 14:54:24 +0100

    fix: rename folders

--- data_loaders/__init__.py
+++ data_loaders/__init__.py
@@ -1,16 +1,6 @@
-"""
-Datasets module for loading and managing datasets.
-
-This module provides builders for various types of datasets:
-- classification_data: For classification tasks (MNIST, CIFAR10, ImageFolder, etc.)
-- diffusion_data: For diffusion models (TODO: future implementation)
-"""
+__all__ = [
+    'datasets_builder',
+    'build_dataset',]
 
 from . import datasets_builder
 from .datasets_builder import build_dataset
-
-
-__all__ = [
-    'datasets_builder',
-    'build_dataset',
-]
